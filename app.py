ae1f9d715b9a5b9be77b9adc8418808e5be282b6
Sai_bhavan <47709071+saiinfy@users.noreply.github.com>
2026-04-01 17:59:01 +0530

    flask app

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-print("this is new app")
+from flask import Flask
 
+app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    # This block is executed when the script is run directly
+    app.run(host="0.0.0.0", port=8000, debug=True)
