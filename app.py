e20a309a1630b33358a1b59c36a2edaf5281954b
Sai_bhavan <47709071+saiinfy@users.noreply.github.com>
2026-04-05 08:52:02 +0530

    yaml changes

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello_world():
 
 if __name__ == "__main__":
     # This block is executed when the script is run directly
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
