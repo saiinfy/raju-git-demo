f799d0c9bead7f8b53f79f71c975aca4000b2e40
Sai_bhavan <47709071+saiinfy@users.noreply.github.com>
2026-04-06 21:10:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, Raju Garu"
 
 if __name__ == "__main__":
     # This block is executed when the script is run directly
